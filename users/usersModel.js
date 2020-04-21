b6704caab216eb8333b77c667fe45a621a8c2157
kp1129 <katya.p.1129@gmail.com>
2020-04-21 15:42:17 -0700

    code cleanup

--- users/usersModel.js
+++ users/usersModel.js
@@ -7,11 +7,6 @@ module.exports = {
     findBy
 }
 
-
-// BEFORE FINAL SUBMIT
-// BE SURE TO SET IT UP SO THAT
-// GETALL
-// DOES NOT RETURN EVERYONE'S PASSWORD LOL
 function getAll() {
     return db('users')
 }
